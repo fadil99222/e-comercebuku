6f4a535ae2a0bb01e3ec8735adf1e9ea9e8eb41f
fadil99222 <fadiljunaldi0308@gmail.com>
2022-06-24 02:44:52 -0500

    menambahkan halaman home

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view ('home',[
+        "icon"=>"img/icon_booku.png"
+
+    ]);
+
 });
